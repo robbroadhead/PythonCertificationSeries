e307ea1ff622764e650a7668dc44f5b368fa55b2
Rob Broadhead <rob@rb-sns.com>
2021-05-31 10:04:21 -0500

    Shortcuts and coparison operators

--- Assignments-Shortcuts.py
+++ Assignments-Shortcuts.py
@@ -2,3 +2,75 @@
 # Shortcuts: +=, -=, *=, /=, %=, //=, **=
 
 print("Assignment Examples")
+
+print("Simple Assignment =")
+a = 3
+b = 6
+print("b after b = 6")
+print(b)
+c = 6 + 7 * 9
+print("c after c = 6 + 7 * 9")
+print(c)
+d = 1
+
+print("b after b += 6 [same as b = b + 6] ")
+b += 6
+print(b)
+print("b after b += 6")
+b += 6
+print(b)
+
+print("b after b -= 6 [same as b = b - 6] ")
+b -= 6
+print(b)
+
+print("b after b *= 6 [same as b = b * 6] ")
+b *= 6
+print(b)
+
+print("b after b /= 6 [same as b = b / 6] ")
+b /= 6
+print(b)
+
+print("b after b %= 6 [same as b = b % 6] ")
+c = b
+c %= 6
+print(c)
+c = b
+c %= 5
+print(c)
+
+print("b after b //= 6 [same as b = b // 6] ")
+c = b
+print(c)
+c //= 6
+print(c)
+c = b
+c //= 3
+print(c)
+
+print("b after b **= 3 [same as b = b ** 3 => b = b * b * b]")
+c = b
+print(c)
+c **= 3
+print(c)
+
+print("Comparison operators")
+print("== >= <= < > !=")
+a = 1
+b = 2
+c = 3
+d = a + b
+e = 10
+print ("a == b")
+print(a == b)
+print ("d == c")
+print(c == d)
+print ("a == b")
+print(a >= b)
+print(a <= b)
+print(c >= d)
+print(c >= a)
+print(c > d)
+print(c != a)
+
